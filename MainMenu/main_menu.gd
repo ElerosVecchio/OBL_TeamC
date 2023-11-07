d3eb8e7cee32ae874379f4ff520ecc11f7d99d6a
Vigneshwar Boggula <vboggula@uci.edu>
2023-11-07 11:35:25 -0800

    Main Menu Functionality Placeholder
    Placeholders for title image, play button, quit button and options button.

---
+++ MainMenu/main_menu.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+@onready var tree = get_tree()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+		pass
+
+
+# Placeholder for play button functionality
+func on_play_button_pressed():
+	print("PLAY button pressed.")
+
+
+# Placeholder for quit button functionality
+func on_quit_button_pressed():
+	print("QUIT button pressed.")
+	# tree.quit()
+
+
+# Placeholder for settings button functionality
+func on_options_button_pressed():
+	print("OPTIONS button pressed.")
+
