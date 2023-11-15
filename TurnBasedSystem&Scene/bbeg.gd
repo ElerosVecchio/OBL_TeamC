bb446d4156c8bb02128cbe65582ff529e0c8af0c
Crab2Cake <orolfocharlie@gmail.com>
2023-11-14 17:46:23 -0800

    Adding Scripts

---
+++ TurnBasedSystem&Scene/bbeg.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+var not_idle = false
+
+func _physics_process(delta):
+	bbeg_animations()
+	update_bbeghealth()
+	$AnimatedSprite2D.flip_h = true
+	bbegattacking()
+	defeat()
+	
+func bbeg_animations():
+	if not_idle == false:
+		$AnimatedSprite2D.play('evil_idle')
+
+func update_bbeghealth():
+	var healthbar = $healthbar
+	healthbar.max_value = Global.bbegmaxhealth
+	healthbar.value = Global.bbegcurrenthealth
+	healthbar.get_node("Label").text = "HP: %d/%d" % [Global.bbegcurrenthealth, Global.bbegmaxhealth]
+
+func bbegattacking():
+	if Global.enemyturn == true:
+		not_idle = true
+		$AnimatedSprite2D.play('evil_attack')
+		await $AnimatedSprite2D.animation_finished
+		not_idle = false
+		Global.enemyturn = false
+		Global.playerturn = true
+		
+func defeat():
+	if Global.bbegcurrenthealth <= 0:
+		Global.bbegdefeated = true
+		$healthbar.hide()
+		not_idle = true
+		$AnimatedSprite2D.play('evil_death')
+		await $AnimatedSprite2D.animation_finished
+		get_tree().quit()
---
+++ TurnBasedSystem&Scene/hero.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+var not_idle = false
+
+func _physics_process(delta):
+	hero_animations()
+	update_health()
+	attacking()
+	defeat()
+	
+func hero_animations():
+	if not_idle == false:
+		$AnimatedSprite2D.play('Idle')
+
+func update_health():
+	var healthbar = $healthbar
+	healthbar.max_value = Global.playermaxhealth
+	healthbar.value = Global.playercurrenthealth
+	healthbar.get_node("Label").text = "HP: %d/%d" % [Global.playercurrenthealth, Global.playermaxhealth]
+
+func attacking():
+	if Global.playerattacking == true:
+		not_idle = true
+		$AnimatedSprite2D.play('Attack')
+		await $AnimatedSprite2D.animation_finished
+		Global.playerattacking = false
+		not_idle = false
+	else:
+		pass
+
+func defeat():
+	if Global.playercurrenthealth <= 0:
+		Global.playerdefeated = true
+		$healthbar.hide()
+		not_idle = true
+		$AnimatedSprite2D.play('Die')
+		await $AnimatedSprite2D.animation_finished
+		get_tree().quit()
