d57d7324ada7d8e94ca80d8df4bb4bff95c11d4d
wqying <qywong@uci.edu>
2023-12-03 04:14:06 -0800

    herowalkinganimation

---
+++ script(walking map)/hero(walking_map).gd
@@ -0,0 +1,69 @@
+extends CharacterBody2D
+
+const speed = 100
+var current_dir = "none"
+
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
+
+func _physics_process(delta):
+	player_movement(delta)
+
+
+func player_movement(delta):
+	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1) # we moving in some direction
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		play_anim(1)
+		current_dir = "left"
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		play_anim(1)
+		current_dir = "down"
+		velocity.y = speed
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_up"):
+		play_anim(1)
+		current_dir = "up"
+		velocity.y = -speed
+		velocity.x = 0
+	else:
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+
+	move_and_slide()
+
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	# playing different animations for different usage
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("side_walking")
+		elif movement == 0:
+			anim.play("front_idle")
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("side_walking")
+		elif movement == 0:
+			anim.play("front_idle")
+	if dir == "up":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("updown_walking")
+		elif movement == 0:
+			anim.play("front_idle")
+	if dir == "down":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("updown_walking")
+		elif movement == 0:
+			anim.play("front_idle")
